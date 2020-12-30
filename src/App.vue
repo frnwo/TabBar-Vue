add56c61c40162e8e012869bdd73188734e042c1
zjh <1366448731@qq.com>
2020-12-30 16:10:50 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
+<script>
+import MainTabBar from './components/tabbar/MainTabBar';
+
+export default {
+  name:'App',
+  components:{
+    MainTabBar,
+   
+  }
+}
+</script>
+   
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+/* 在style引用css前面需要加@ ，在js文件里就直接 import  */
+    @import "./assets/css/base.css";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/tabbar/MainTabBar.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+  <tab-bar>
+      <!-- 因为"/home"是固定值，不是变量，所以path前面不用加: -->
+      <tab-bar-item path="/home" activeColor="blue">
+        
+        <img class="" slot="item-icon" src="~assets/img/tabbar/home.svg"/>
+        <img class="" slot="item-icon-active" src="~assets/img/tabbar/home_active.svg"/>
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="blue">
+        <img class="" slot="item-icon" src="~assets/img/tabbar/category.svg"/>
+         <img class="" slot="item-icon-active" src="~assets/img/tabbar/category_active.svg"/>
+        <div  slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="blue">
+        <img class="" slot="item-icon" src="~assets/img/tabbar/cart.svg"/>
+         <img class="" slot="item-icon-active" src="~assets/img/tabbar/cart_active.svg"/>
+        <div  slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" activeColor="blue">
+        <img class="" slot="item-icon" src="~assets/img/tabbar/profile.svg"/>
+         <img class="" slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg"/>
+        <div  slot="item-text">我的</div>
+      </tab-bar-item>
+      
+    </tab-bar>
+</div>
+</template>
+
+<script>
+import TabBar from './TabBar';
+import TabBarItem from './TabBarItem';
+
+export default {
+  components:{
+    TabBar,
+    TabBarItem,
+  }
+}
+</script>
+
+<style>
+
+</style>
